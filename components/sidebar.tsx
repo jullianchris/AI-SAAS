4c26d126f265767b339b9313721dc4305801c041
jullianchris <jullianchristopher6@gmail.com>
2024-03-15 23:08:14 +0530

    dashboard page design implemented

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 import { Montserrat } from "next/font/google";
 import { cn } from "@/lib/utils";
 import { Code, ImageIcon, LayoutDashboard, MessageSquare, Music, Settings, VideoIcon } from "lucide-react";
+import { usePathname } from "next/navigation";
 
 const poppins = Montserrat ({ weight: '600', subsets: ['latin'] });
 
@@ -54,6 +55,9 @@ const routes = [
 ];
 
 const sidebar = () => {
+
+  const pathname = usePathname();
+  
   return (
     <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
         <div className="px-3 py-2 flex-1">
@@ -72,7 +76,7 @@ const sidebar = () => {
               href={route.href}
               className={cn(
                 "text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition",
-               
+                pathname === route.href ? "text-white bg-white/10" : "text-zinc-400",
               )}
             >
               <div className="flex items-center flex-1">
