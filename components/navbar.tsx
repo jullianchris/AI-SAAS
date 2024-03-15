5d133f37baebdd5c0db21e8b012cbd6f7c4c37d6
jullianchris <jullianchristopher6@gmail.com>
2024-03-15 21:19:20 +0530

    basic layout works completed

---
+++ components/navbar.tsx
@@ -0,0 +1,15 @@
+import {Button} from  "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
+import { Menu } from "lucide-react";
+import MobileSidebar from "@/components/mobile-sidebar";
+
+export default function Navbar() {
+  return (
+    <div className='flex items-center p-4'>
+        <MobileSidebar/>
+        <div className="flex w-full justify-end">
+            <UserButton afterSignOutUrl="/"/>
+        </div>
+    </div>
+  )
+}
